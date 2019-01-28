02c7c552c6db20088d41003a14d5275701747761
Sivonja8 <47109267+Sivonja8@users.noreply.github.com>
2019-01-28 17:48:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -302,8 +302,7 @@ client.on('message', message => {
         .addField(':tada: **resume**', 'Resume paused music.')
         .addField(':tada: **queue**', 'View the queue.')
         .addField(':tada: **np**', 'Shows what song the bot is currently playing.')
-.setfooter(text='Just-Some-Bots/MusicBot ({})', icon_url='https://i.imgur.com/gFHBoZA.png')
-        .setauthor(icon_url=self.user.avatar_url)
+
 //        .setFooter('#Logaut', message.author.displayAvatarURL)
 	.setColor("6f39e5")
       message.channel.send(helpEmbed);
