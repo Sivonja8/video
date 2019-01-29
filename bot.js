9f61ca8175d72a4e0efeb8e02b97e787b50661e5
Sivonja8 <47109267+Sivonja8@users.noreply.github.com>
2019-01-29 15:01:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,15 @@ const prefix = "-";
 ////////////////////////
 client.on("ready", () => {
     client.user.setActivity("-help | rujtex.eu", { type: "WATCHING"})
+	const channel = client.channels.get("mychannelid");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
 });
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
